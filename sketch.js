f941dfd5c43486c411eaa2fb8bcba5c6e0bb1645
naomihnguyen05 <100325630+naomihnguyen05@users.noreply.github.com>
2022-05-09 23:50:23 -0700

    Ninth Commit
    Updated with star buttons to showcase my artwork in switch statement., clickables, assets, and description. 2 out of 7 were completed.

--- sketch.js
+++ sketch.js
@@ -4,11 +4,15 @@ let as;
 // let db;
 // images
 let sm;
+let ag;
+let mb;
 // button images
 var galleryImg;
 var asImg;
 var homeImg;
 var playImg;
+var addImg;
+var xImg;
 // stars
 let stars = []
 // switch statement
@@ -37,9 +41,15 @@ function preload() {
   asImg = loadImage('assets/asbutton-05.png');
   homeImg = loadImage('assets/homebutton-06.png');
   playImg = loadImage('assets/playbutton-07.png');
+  addImg = loadImage('assets/addbutton-08.png');
+  xImg = loadImage('assets/xbutton-10.png');
   // music
-  soundBack = new Howl({ src: ['assets/solitude.mp3'], loop: true, volume: 0.03});
-  soundBack.play();
+  // soundBack = new Howl({ src: ['assets/solitude.mp3'], loop: true, volume: 0.03});
+  //soundBack.play();
+  // artwork pictures
+  ag = loadImage('assets/andrewgarfieldport.jpg');
+  mb = loadImage('assets/mannequinbodies.jpeg');
+
 }
 
 
@@ -56,6 +66,7 @@ function setup() {
 }
 }
 
+// doodling play screen
 function drawPencil(pensize,x,y) {
   strokeWeight(pensize);
   stroke(r,g,b);
@@ -239,6 +250,7 @@ function drawButtons() {
     }
 }
 
+// flashing stars
 function drawStars() {
   for (i = 0; i < 500; i++){
     let x = stars[i].x;
@@ -248,6 +260,7 @@ function drawStars() {
 	}
 }
 
+// Born to Be text
 function titleScreen() {
   fill(255);
   textSize(50);
@@ -256,6 +269,7 @@ function titleScreen() {
   textFont(myFontb);
 }
 
+// switch statements
 function draw() {
   switch (gameState) {
     case 'start':
@@ -271,6 +285,13 @@ function draw() {
     click3.draw();
     click4.draw();
     click7.draw();
+    click8.draw();
+    click9.draw();
+    click10.draw();
+    click11.draw();
+    click12.draw();
+    click13.draw();
+    click14.draw();
     break;
     case 'artist':
     image(as, 0, 0, 1366, 768);
@@ -284,7 +305,86 @@ function draw() {
     drawRainbow();
     drawButtons();
     click6.draw();
+    break;
+    case 'popupart1':
+    popupArt1();
+    break;
+    case 'popupart2':
+    popupArt2();
+    break;
+    case 'popupart3':
+    popupArt3();
+    break;
+    case 'popupart4':
+    popupArt4();
+    break;
+    case 'popupart5':
+    popupArt5();
+    break;
+    case 'popupart6':
+    popupArt6();
+    break;
+    case 'popupart7':
+    popupArt7();
+    break;
   }
+}
+
+// pop up art screens
+function popupArt1() {
+  push();
+  rectMode(CENTER);
+  fill(255);
+  strokeWeight(0.7);
+  stroke(150);
+  rect(685, 400, 700, 500, 10);
+  textAlign(CENTER);
+  textSize(16);
+  textFont('myFontb');
+  fill(0);
+  text('ANDREW GARFIELD PORTRAIT',830, 280);
+  imageMode(CENTER);
+  image(ag, 530, 420, 252, 336);
+  textAlign(LEFT);
+  textSize(13);
+  textFont('myFontb');
+  fill(0);
+  text('This is an Andrew Garfield portrait I drew',710, 320);
+  text('using charcoal and eraser. I based this drawing',710, 335);
+  text('off of a magazine layout photo I found of him.',710, 350);
+  text('I learned how to visualize shapes and lines from',710, 365);
+  text('a small photo in order to expand it into a larger',710, 380);
+  text('drawing. This was drawn on an 18 x 24 inch sheet.',710, 395);
+  pop();
+  click15.draw();
+}
+
+function popupArt2() {
+  push();
+  rectMode(CENTER);
+  fill(255);
+  strokeWeight(0.7);
+  stroke(150);
+  rect(685, 400, 700, 500, 10);
+  textAlign(CENTER);
+  textSize(16);
+  textFont('myFontb');
+  fill(0);
+  text('MANNEQUIN BODIES',830, 280);
+  imageMode(CENTER);
+  image(mb, 530, 420, 252, 336);
+  textAlign(LEFT);
+  textSize(13);
+  textFont('myFontb');
+  fill(0);
+  text('This is a charcoal drawing I drew of a body of',710, 320);
+  text('a mannequin. The mannequin was placed under',710, 335);
+  text('studio lighting in order to give it shadow and',710, 350);
+  text('highlight. I paid very close attention to detail',710, 365);
+  text('during this because I wanted to replicate the exact',710, 380);
+  text('light, shade, and curvy features of it!',710, 395);
+  pop();
+  click15.draw();
 }
 
 // clickables
@@ -359,5 +459,85 @@ click7.cornerRadius = 100;
 click7.onRelease = function () {
   drawStart = true;
   gameState = 'play'
+}
+click8 = new Clickable();
+click8.image = addImg;
+click8.locate (300, 250);
+click8.resize(30, 30);
+click8.text = "  ";
+click8.strokeWeight = 0;
+click8.cornerRadius = 100;
+click8.onRelease = function () {
+  gameState = 'popupart1'
+}
+click9 = new Clickable();
+click9.image = addImg;
+click9.locate (400, 200);
+click9.resize(30, 30);
+click9.text = "  ";
+click9.strokeWeight = 0;
+click9.cornerRadius = 100;
+click9.onRelease = function () {
+  gameState = 'popupart2'
+}
+click10 = new Clickable();
+click10.image = addImg;
+click10.locate (620, 300);
+click10.resize(30, 30);
+click10.text = "  ";
+click10.strokeWeight = 0;
+click10.cornerRadius = 100;
+click10.onRelease = function () {
+  gameState = 'popupart3'
+}
+click11 = new Clickable();
+click11.image = addImg;
+click11.locate (480, 530);
+click11.resize(30, 30);
+click11.text = "  ";
+click11.strokeWeight = 0;
+click11.cornerRadius = 100;
+click11.onRelease = function () {
+  gameState = 'popupart4'
+}
+click12 = new Clickable();
+click12.image = addImg;
+click12.locate (750, 150);
+click12.resize(30, 30);
+click12.text = "  ";
+click12.strokeWeight = 0;
+click12.cornerRadius = 100;
+click12.onRelease = function () {
+  gameState = 'popupart5'
+}
+click13 = new Clickable();
+click13.image = addImg;
+click13.locate (950, 530);
+click13.resize(30, 30);
+click13.text = "  ";
+click13.strokeWeight = 0;
+click13.cornerRadius = 100;
+click13.onRelease = function () {
+  gameState = 'popupart6'
+}
+click14 = new Clickable();
+click14.image = addImg;
+click14.locate (950, 320);
+click14.resize(30, 30);
+click14.text = "  ";
+click14.strokeWeight = 0;
+click14.cornerRadius = 100;
+click14.onRelease = function () {
+  gameState = 'popupart7'
+}
+click15 = new Clickable();
+click15.image = xImg;
+click15.locate (355, 170);
+click15.resize(30, 30);
+click15.text = "  ";
+click15.strokeWeight = 0;
+click15.cornerRadius = 100;
+click15.onRelease = function () {
+  gameState = 'main'
 }
 }
